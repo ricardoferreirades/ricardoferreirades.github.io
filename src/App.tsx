311820dc124c66404288d11be90e773d75106805
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-01 14:12:26 +0200

    chore(components): build the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import CardsSection from "./components/CardsSection";
+import Header from "./components/Header";
+import TopBar from "./components/TopBar"; 
+
 function App() {
-  return <h1>Ricardo Ferreira</h1>;
+  return <main className="container mx-auto">
+    <TopBar />
+    <Header />
+    <CardsSection />
+  </main>;
 }
 
 export default App;
