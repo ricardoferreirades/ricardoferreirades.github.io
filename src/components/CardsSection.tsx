33f4c251d4543d33e5849da5a3631957bb271b1c
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-01 14:11:40 +0200

    chore(components): add the cards section component

---
+++ src/components/CardsSection.tsx
@@ -0,0 +1,40 @@
+import Card, { CardProps } from "./Card"
+
+  export default function CardsSection() {
+    const cardDataList: Array<CardProps> = [
+      {
+        icon: {
+          src: '/assets/images/pencil.svg',
+          alt: 'pencil icon which representes creativity'
+        },
+  
+        title: 'UI & UX',
+        description: 'Creating user interfaces and experiences that enable easy accessibility.'
+      },
+      {
+        icon: {
+          src: '/assets/images/code.svg',
+          alt: 'pencil icon which representes creativity'
+        },
+        title: 'Development',
+        description: 'Bringing ideas to life using the most advanced technologies.'
+      },
+      {
+        icon: {
+          src: '/assets/images/group.svg',
+          alt: 'pencil icon which representes creativity'
+        },
+        title: 'Collaboration',
+        description: 'Playing an encouraging role to maintain a good culture overall.'
+      }
+    ]
+    return <section className="flex justify-center items-center gap-8 max-w-[733px] mx-auto">
+      {
+        cardDataList.map(({title, icon, description}) => {
+          return <Card key={icon.src} title={title} icon={icon} description={description} />
+        })
+      }
+    </section>
+  }
+  
+  
