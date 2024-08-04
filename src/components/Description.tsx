b6975e92b7160b82b33bbd22e0e5bd04aba3a353
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-04 11:09:16 +0200

    chore(style): make the header section responsive

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,5 +1,5 @@
 export default function Description() {
-    return <h1 className="text-[3rem] text-center text-light-gray max-w-[687px] leading-[3.75rem]">
+    return <h1 className="text-2xl md:text-4xl lg:text-[3rem] text-center text-light-gray lg:max-w-[687px] lg:leading-[3.75rem] max-w-[600px]">
       Designing, building, and pushing solutions to the next level
     </h1>
   }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import Description from "./Description";
 
 
 export default function Header() {
-    return <header className="flex flex-col items-center gap-6 mx-auto py-[7.25rem]">
+    return <header className="flex flex-col items-center gap-6 mx-auto py-10 lg:py-[7.25rem]">
       <Avatar />
-      <h2 className="text-medium-gray-2 text-xl">Ricardo Ferreira - Software Developer</h2>
+      <h2 className="text-medium-gray-2 lg:text-xl">Ricardo Ferreira - Software Developer</h2>
       <Description />
     </header>
   };
