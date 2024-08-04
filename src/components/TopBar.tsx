00f640e09246d957db63620c1ab1a0aac0bab042
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-04 10:54:34 +0200

    chore(responsiveness): make the topbar reponsive

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 export default function TopBar() {
-    return <section className="flex justify-between items-center gap-3 border-b border-dark-gray-3 py-6 text-xs text-dark-gray-5">
+    return <section className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-8 md:gap-3 border-b border-dark-gray-3 py-6 text-md lg:text-xs text-dark-gray-5">
       <a href="mailto:contact@rferreira.dev" className="inline-flex items-center gap-3 hover:text-light-gray"><img src="/assets/images/envelope.svg" alt="an envelop icon which represents a contact near the email link" />contact@rferreira.dev</a>
-      <menu className="flex gap-6">
+      <menu className="flex justify-between gap-20">
         <a href="#" className="hover:text-light-gray">Projects</a>
         <a href="https://docs.google.com/document/d/1xC9EIEc4SiMVMkZIlG9Dq33Oen12Q2jI1pkZ9YXS5sQ/edit?usp=sharing" target="_blank" title="Curriculum Vitae"  className="hover:text-light-gray">CV</a>
         <a href="https://github.com/ricardoferreirades" className="hover:text-light-gray">Github</a>
