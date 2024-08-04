b254af90a4c124de54284f970256da2eb28101b9
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-04 12:34:05 +0200

    chore(style): add roundness to avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,5 @@
 export default function Avatar() {
-    return <figure>
+    return <figure className="rounded-full overflow-hidden">
       <img src='/assets/images/Avatar.png' width='155' height='155' alt="Ricardo Ferreira photo" />
     </figure>
   }
