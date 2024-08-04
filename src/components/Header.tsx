a695c5955454990016b9d332bbefe11157f9bb1a
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-04 10:32:49 +0200

    chore(style): add header classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Description from "./Description";
 export default function Header() {
     return <header className="flex flex-col items-center gap-6 mx-auto py-[7.25rem]">
       <Avatar />
-      <h2>Ricardo Ferreira - Software Developer</h2>
+      <h2 className="text-medium-gray-2 text-xl">Ricardo Ferreira - Software Developer</h2>
       <Description />
     </header>
   };
