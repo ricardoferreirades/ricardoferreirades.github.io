f68309d7a9fcf22ec3c68993c905b4e47476f216
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-04 10:33:39 +0200

    refactor(style): topbar
    add classes, remove not active links

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,14 +1,10 @@
 export default function TopBar() {
-    return <section className="flex justify-between items-center gap-3 border-b py-6 text-xs">
-      <a href="mailto:contact@rferreira.dev" className="inline-flex items-center gap-3"><img src="/assets/images/envelope.svg" alt="an envelop icon which represents a contact near the email link" />contact@rferreira.dev</a>
+    return <section className="flex justify-between items-center gap-3 border-b border-dark-gray-3 py-6 text-xs text-dark-gray-5">
+      <a href="mailto:contact@rferreira.dev" className="inline-flex items-center gap-3 hover:text-light-gray"><img src="/assets/images/envelope.svg" alt="an envelop icon which represents a contact near the email link" />contact@rferreira.dev</a>
       <menu className="flex gap-6">
-        <a href="">Portfolio</a>
-        <a href="">Youtube</a>
-        <a href="">Medium</a>
-        <a href="">Hacker Hank</a>
-        <a href="">Github</a>
-        <a href="">CV Download</a>
-        <a href="">DE</a>
+        <a href="#" className="hover:text-light-gray">Projects</a>
+        <a href="https://docs.google.com/document/d/1xC9EIEc4SiMVMkZIlG9Dq33Oen12Q2jI1pkZ9YXS5sQ/edit?usp=sharing" target="_blank" title="Curriculum Vitae"  className="hover:text-light-gray">CV</a>
+        <a href="https://github.com/ricardoferreirades" className="hover:text-light-gray">Github</a>
       </menu>
     </section>
   }
