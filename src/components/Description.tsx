714435f8d9c4742133167ebb40c4e50fac274b76
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-04 10:32:37 +0200

    chore(style): add description classes

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,5 +1,5 @@
 export default function Description() {
-    return <h1 className="text-[3rem] text-center max-w-[687px] leading-[3.75rem]">
+    return <h1 className="text-[3rem] text-center text-light-gray max-w-[687px] leading-[3.75rem]">
       Designing, building, and pushing solutions to the next level
     </h1>
   }
