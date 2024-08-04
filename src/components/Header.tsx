acc2e463b63e591c27f9f1f2372a07de78c4a0b6
Ricardo Ferreira <ricardoferreirades@gmail.com>
2024-08-04 11:22:56 +0200

    chore(style):  adjust header spacing on tablet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Description from "./Description";
 
 
 export default function Header() {
-    return <header className="flex flex-col items-center gap-6 mx-auto py-10 lg:py-[7.25rem]">
+    return <header className="flex flex-col items-center gap-6 mx-auto py-10 lg:py-15 xl:py-[7.25rem]">
       <Avatar />
       <h2 className="text-medium-gray-2 lg:text-xl">Ricardo Ferreira - Software Developer</h2>
       <Description />
